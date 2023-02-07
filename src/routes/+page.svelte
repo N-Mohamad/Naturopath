703db0de7bf5add555fae0774dd134b2b3ed8eed
N-Mohamad <101737216+N-Mohamad@users.noreply.github.com>
2023-02-07 20:22:57 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 
+<script>
+	import Footer from "./footer.svelte";
 
+</script>
 <head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
-
-
 <div class="container">
     <div class="text-column">
         <h1>Welcome to Emma Moorby Naturopathy</h1>
@@ -13,51 +14,192 @@
 <br><br>
         Are you:</p>
         <div class="icon-w">
-            <span class="fa-solid fa-circle-arrow-right" style="font-size: 36px; margin-right: 10px;"></span>
+            <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
             <span>Struggling because doctors haven’t been able to identify the cause of your symptoms?</span>
         </div>
         <div>
         <br> <br>
-        <span class="fa-solid fa-circle-arrow-right" style="font-size: 36px; margin-right: 10px;"></span>
+        <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
         <span>Sick and tired of feeling ‘sick and tired?</span>
 </div>
 <br>
     <div>
-<span class="fa-solid fa-circle-arrow-right" style="font-size: 36px; margin-right: 10px;"></span>
+<span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
         <span>Medicated but still developing symptoms?</span>
     </div>
+    <br>
+    <h3>I Can Help.</h3>
 </div>
-
-
     <div class="image-column">
     <img src="https://www.emmamoorbynd.com/wp-content/uploads/bb-plugin/cache/ezgif.com-gif-maker4-1536x1304-square.jpg"  alt="image">
     </div>
     </div>
+                                                        <!-- I support clients with: -->
+    <div class="container1">
+        <div class="header-column-support">
+        <h1 class="support-h1">I support clients with:</h1><hr class="support-hr">
+    </div>
+                                                        <!-- left column -->
+        <div class="head-left-right">
+        <div class="left-column"> 
+            <div class="left-head-support">      
+            <img class="img-left" src="https://www.emmamoorbynd.com/wp-content/uploads/bb-plugin/cache/ezgif.com-gif-maker2-1-150x150-circle.jpg" alt="">
+            <div class="left-icon-text">
+                <h3 class="left-right-column-text">Thyroid conditions:</h3>
+            <p class="left-right-column-text">Including</p>
+            <hr class="left-hr-head">
+        </div></div>     
+            <div class="icon-left">
+                <div>
+                <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                <span>Hypothyroidism</span> <br></div>
+                <div>
+                <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                <span>Hashimoto’s Autoimmune Thyroiditis</span></div>
+                <div>
+                <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                <span>Thyroid Nodules</span></div>
+                <div>
+                <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                <span>Post-Partum Thyroiditis</span></div>
+            </div>
+        </div>
+                                                        <!-- right column -->
+        <div></div>
+        <div class="right-column">
+            <div class="right-head-support">
+            <img class ="img-right" src="https://www.emmamoorbynd.com/wp-content/uploads/bb-plugin/cache/ezgif.com-gif-maker3-1-150x150-circle.jpg" alt="">
+            <div class="right-icon-text">
+                <h3 class="left-right-column-text">Hormone Imbalance:</h3>
+            <p class="left-right-column-text">Including</p>
+            <hr class="right-hr-head">
+        </div></div>
+            <div class="icon-right">
+                <div>
+                <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                <span>Infertility and fertility issues</span></div>
+                <div>
+                <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                <span>PCOS (Polycystic ovary syndrome)</span></div>
+                <div>
+                <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                <span>Irregular periods</span></div>
+                <div>
+                <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                <span>Menopause</span> <br></div>
+                <div>
+                <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                <span>Perimenopause</span></div>
+                <div>
+                <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                <span>Endometriosis</span></div>
+                <div>
+                <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                <span>Fibroids</span></div>
+            </div>
+        </div>
+        </div>
+    </div>
+
+
+
+
+    <div class="container2">
+        <div class="left-div-2">
+            <p class="left-div-paragraph">My personal experience of dealing with hypothyroidism, PCOS and infertility, combined with my specialist clinical knowledge, means I truly understand what living with a thyroid health condition means and all its implications. <br><br>
+
+                I have been exactly where you are now and continue to work on myself to be the most optimally healthy version of myself I can be. I consider this a continuous journey. <br><br>
+                
+                It is my mission to empower as many people as possible with thyroid and hormone health conditions to regain their vitality and take back control of their lives.</p>
+        </div>
+
+
+        <div class="right-div-2">
+            <p>I also support <span style="color: yellowgreen;"> Thyroid and Breast Cancer</span>, (please click on the highlighted text for further information.)</p>
+            <div class="inside-right-div2">
+                <h2>Book a free discovery call</h2>
+                <p class="pright-div">Book a free discovery call to discuss how we can work together.</p>
+                <div class="div-book-btn">
+                    <button class="book-btn">Book now</button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="container3">
+        <div>
+            <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+            <span>NB Always seek traditional medical advice with any diagnosed health condition first before exploring natural health options. <br>
+                I work alongside medical doctors and will liaise with them at your request.</span>
+        </div>
+    </div>
+
+    <div class="container4">
+        
+        <div class="anpimg">
+            <img src="https://www.emmamoorbynd.com/wp-content/uploads/2022/09/2020-members-badge.webp" alt="anpimg">
+        </div>
+        <div class="gncimg">
+            <img src="https://www.emmamoorbynd.com/wp-content/uploads/2022/09/GNC-LOGOnew-1024x305-1.webp" alt="gncimg">
+        </div>
+        <dir class="ecoimg">
+            <img src="https://www.emmamoorbynd.com/wp-content/uploads/2022/09/Screenshot-2021-06-02-155703.webp" alt="ecoimg">
+        </dir>
+    </div>
+
+
+    <section>
+        <!-- <img src="https://layalina.awicdn.com/site-images/sites/default/files/prod/article/b/5/439941/06c6a66dacfb42e98632ab3c882074fe041d0368-191221212453.jpg?preset=v4.0_368X207&save-png=1&rnd=1519151RND220215&croptop=1" alt=""> -->
+    </section>
+
+
+    <div class="container5" >
+        <div class="post">
+            <div class="post-header">
+                <div class="left-head">
+                    <a href="#">
+                        <img class="auth-post" src="https://img.freepik.com/free-photo/purple-osteospermum-daisy-flower_1373-16.jpg" alt="">
+
+                    </a>
+                    <div class="post-author">
+                        <span><a href="#"><h4> Emma Moorby Naturopathy Ltd</h4></a>
+                            I am Naturopath dedicated to optimising Thyroid and Hormone Health with simple natural approaches. <br>
+                            
+                            330 people like this</span>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+
 
 <style>
+    /* Welcome to Emma Moorby Naturopathy */
 .container {
     display: flex;
     flex-direction: row;
     width: 1200px;
     margin: 0 auto;
 }
-
 .text-column {
     width: 75%;
     padding: 20px;
     order: -1;
 }
-
 .image-column {
     width: 75%;
     padding: 20px;
 }
-
 img {
     width: 100%;
     height: auto;
 }
-
 hr{
     width: 150px;
     position: relative; right: 100px;
@@ -76,15 +218,219 @@ p{
     line-height: 1.9;
     
 }
-
-.icon-w{
-    display: flex;
-    align-items: center;
-}
 .fa-solid {
 font-size: 36px;
 margin-right: 10px;
 vertical-align: middle;
 color: rgb(177, 225, 195);
 }
+h3{
+    font-family: 'Poppins', sans-serif;
+    font-weight: 300;
+    color: #535555;
+    line-height: 1.2;
+
+}
+/* I support clients with: */
+.container1{
+    display: flex;
+    width: 100%;
+    background-color: #F4F5F0;
+    margin: 0 auto;
+    height: 700px;
+    flex-direction: column;
+}
+
+.header-column-support{
+    display: flex;
+    flex-direction: row;
+    z-index: 2;
+    margin-top: 20px;
+    height: 150px;
+    display: table;
+}
+.head-left-right{
+    display: flex;
+    flex-direction: row;
+}
+.support-h1{
+    text-align-last: center;
+}
+.left-column{
+    background-color: #8fab72;
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+    width: 30%;
+    height: 350px;
+    position:relative; left:80px;
+    padding: 50px;
+    z-index: 1;
+}
+.right-column{
+    background-color: #8fab72;
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+    width: 30%;
+    height: 350px;
+    position:relative; right:80px;
+    padding: 50px;
+    z-index: 1;
+}
+.support-hr{ 
+    width:25%;
+    margin: auto;
+    position: relative;left: 0px;
+}
+.img-left{
+    border-radius: 50%;
+    display: flex;
+    width: 100px;
+    height: 100px;
+    
+}
+.img-right{
+    border-radius: 50%;
+    display: flex;
+    width: 100px;
+    height: 100px;
+}
+.icon-left{
+    display: flex;
+    flex-direction: column;
+    justify-content: left;
+    color: white;
+    
+}
+.icon-right{
+    display: flex;
+    flex-direction: column;
+    justify-content: left;
+    color: white;
+}
+.left-icon-text{
+    margin-left: 10px;
+}
+.right-icon-text{
+    margin-left: 10px;
+}
+.left-head-support{
+    display: flex;
+    flex-direction: row;
+}
+.right-head-support{
+    display: flex;
+    flex-direction: row;
+    margin-top: 15px;
+}
+.left-right-column-text{
+    color: white;
+}
+
+.right-hr-head{
+    width: 350px;
+    position:relative; left:0; top:-20px;
+    height: 1px;
+    background-color: #ccc;
+}
+.left-hr-head{
+    width: 350px;
+    position:relative; left:0; top:-20px;
+    height: 1px;
+    background-color: #ccc;
+}
+
+.container2{
+    display: flex;
+    width: 1200px;
+    margin: 0 auto;
+}
+.left-div-2{
+    width: 50%;
+    padding: 20px;
+}
+.left-div-paragraph{
+    font-style: oblique;
+}
+.right-div-2{
+    width: 50%;
+    padding: 20px;
+}
+
+.inside-right-div2{
+    display: flex;
+    width: 100%;
+    background-color: #c8ccb4;
+    height: 180px;
+    flex-direction: column;
+    text-align: center;
+    position: relative;
+    color: white;
+    font-family: Georgia, serif;
+    padding: 20px;
+}
+.div-book-btn{
+    justify-content: center;
+    align-items: center;
+}
+.book-btn{
+    margin: 0;
+    border-radius: 22px;
+    width: 150px;
+    height: 50px;
+    background-color: #8fab72;
+    color: white;
+    border: none;
+
+}
+.pright-div{
+    color: black;
+}
+
+
+.container3{
+    width: 1200px;
+    display: flex;
+    margin: 0 auto;
+}
+.container4{
+    width: 1200px;
+    display: flex;
+    margin: 0 auto;
+}
+.anpimg{
+    width: 300px;
+    padding: 30px;
+    margin: 0 auto;
+}
+.gncimg{
+    width: 300px;
+    padding: 30px;
+    margin: 0 auto;
+}
+.ecoimg{
+    width: 300px;
+    padding: 30px;
+    margin: 0 auto;
+}
+.container5{
+    background: gray;
+    display: flex;
+    box-sizing: border-box;
+    border: 20px;
+    width: 100%;
+    max-width: 60rem;
+    height: 200px;
+}
+.auth-post{
+    width: 4rem;
+    height: 4rem;
+    border-radius: 50%;
+}
+.left-head{
+    box-sizing: border-box;
+    border: 20px;
+}
+
 </style>
