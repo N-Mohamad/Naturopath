d29283b8493975405f7af6fbd79ce37227092a19
N-Mohamad <101737216+N-Mohamad@users.noreply.github.com>
2023-02-07 20:24:36 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
     import Navbar from "./Navbar.svelte";
     import Hero from "./Hero.svelte";
+    import Footer from "./footer.svelte";
 </script>
 
 <Hero />
 <Navbar />
 
 <slot />
-
+<Footer />
 
