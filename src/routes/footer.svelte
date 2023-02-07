e19cb88e117bb19cbb69099681cb9d5b6980ccc4
N-Mohamad <101737216+N-Mohamad@users.noreply.github.com>
2023-02-07 20:21:07 +0100

    Add files via upload

---
+++ src/routes/footer.svelte
@@ -0,0 +1,60 @@
+<footer>
+    <div class="container-footer">
+        
+        <div class="left-footer">
+            <img src="https://www.emmamoorbynd.com/wp-content/uploads/2020/11/footer_logo.png" alt="">
+
+        </div>
+        <div class="right-footer">
+            <h1>Hi I'm Emma</h1>
+            <p>I’m Emma, registered naturopath, and nutritional therapist, specialising in thyroid and hormone health. Find out about my personal experience of PCOS, fertility issues and thyroid imbalance which prompted me to work in this area click here. <br><br>
+
+                I have a qualification in Naturopathy and Nutritional therapy from the College of Naturopathic Medicine in London and a background in recipe and product development. <br><br>
+                
+                In addition, I have completed additional training in Cancer and Integrative Oncology with the Peat Institute and enrolled in their mentorship programme. This ensures that I keep up to date with the latest research, I also have the support of Patricia Peat who has many years of experience of supporting clients with cancer. <br><br>
+                
+                When not supporting clients on a 1:1 basis or mentoring, I also provide lectures at health conferences.</p>
+
+        </div>
+        
+
+    </div>
+    <!-- <div class="bottom-footer">
+            <div class="copyright">Copyright 2023 Mohamad Naksho</div>
+        </div> -->
+</footer>
+
+
+<style>
+    .container-footer{
+        display: flex;
+        flex-direction: row;
+        background-color: #8fab72;
+        width: 100%;
+        height: 500px;
+        color: white;
+        margin: 0 auto;
+    }
+    .left-footer{
+        width: 40%;
+        padding-left:10%;
+        padding-top: 5% ;
+    }
+
+    .right-footer{
+        width: 60%;
+        padding-top: 6%;
+        padding-right: 150px ;
+    
+    }
+
+
+
+    /* .copyright{
+        color: #ffffff;
+        font-size: 14px;
+        display: flex;
+        padding: 20px;
+        border-bottom: 1px solid #ddd;
+    } */
+</style>
