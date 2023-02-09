2d71a0fbc439f8649c21f61921b105dcb9dcf410
N-Mohamad <101737216+N-Mohamad@users.noreply.github.com>
2023-02-09 01:14:42 +0100

    Update +page.svelte

--- src/routes/Blog/+page.svelte
+++ src/routes/Blog/+page.svelte
@@ -1,1 +1,59 @@
-<div class="container">sercices</div>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<div class="head-page">
+    <h1 class="h1-head">BLOG</h1>
+    <br>
+</div>
+<div class="container">
+    <div class="text-column">
+        <h2 class="container1-h2">Why consult with a Naturopath?</h2>
+        <p>By Emma | Jul 18, 2022 | Uncategorised</p>
+        <div class="icon-w">
+            <p class="container-span" >A Naturopath combines a range of natural therapies In short a Naturopath is a Nutritional Therapist , Homeopath, and Herbalist Seeing a Naturopath is very different to an appointment with ...</p>
+</div>
+<br>
+</div>
+    <div class="image-column">
+    <img src="https://www.emmamoorbynd.com/wp-content/uploads/2022/07/brooke-lark-kXQ3J7_2fpc-unsplash-scaled-1-650x350.jpg"  alt="image">
+    </div>
+    </div>
+    <br><br><br>
+    <style>
+        .container {
+    display: flex;
+    flex-direction: row;
+    width: 1200px;
+    margin: 0 auto;
+}
+.container1-h2{
+    font-family:'Courier New', Courier, monospace;
+    font-weight: 400;
+}
+.text-column {
+    width: 75%;
+    padding: 20px;
+    
+}
+.image-column {
+    width: 75%;
+    padding: 20px;
+    order: -1;
+}
+img {
+    width: 100%;
+    height: auto;
+}
+.container-span{
+    font-size: 1.5em;
+}
+.head-page{
+    display: flex;
+    margin: 0 auto;
+    width: 1200px;
+}
+.h1-head{
+    font-family:'Courier New', Courier, monospace;
+    font-weight: 400;
+}
+</style>
