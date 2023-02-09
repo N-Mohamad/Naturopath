f7ce151de67b8a8bc55d421c6c13f51333cb3772
N-Mohamad <101737216+N-Mohamad@users.noreply.github.com>
2023-02-09 01:16:23 +0100

    Update +page.svelte

--- src/routes/Naturopath/+page.svelte
+++ src/routes/Naturopath/+page.svelte
@@ -1,1 +1,362 @@
-<div class="container">About</div>
+<head>
+    
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+
+
+<div class="container">
+    <div class="text-column">
+        <h1>How Naturopathy can help you</h1>
+        <hr class="hr-natur">
+        <br><br>
+        <div class="icon-w">
+            <p class="container-span" >Naturopathy combines and uses natural remedies, such as herbs, nutrition, mineral therapy, homeopathy, and lifestyle recommendations to promote positive results.
+                <br><br>
+                The goal of a Naturopath is to locate the underlying factors that are creating disturbance and balance them, thus allowing the innate ability of the person to restore equilibrium. Your body does the work. <br><br>
+                
+                My main focus as a Naturopath is to work with the whole person as an individual, I take a detailed case history during the consultation process, I am looking at removing barriers to vitality, while supporting the body’s inherent self-healing mechanisms and nourishing the body with what it needs. I also consider rebalancing and strengthening vital systems in the body.</p>
+</div>
+<br>
+</div>
+    <div class="image-column">
+    <img src="https://www.emmamoorbynd.com/wp-content/uploads/bb-plugin/cache/ezgif.com-gif-maker3-1-1536x1536-square.jpg"  alt="image">
+    </div>
+    </div>
+
+
+    <div class="container2">
+        <div class="left-div-2">
+            <p class="left-div-paragraph">I include in my consultations and therapeutic plans, naturopathic tools, and recommendations such as herbals, homeopathy, mineral therapy, or stress management techniques to support your body’s vitality.</p>
+        </div>
+
+
+        <div class="right-div-2">
+            <p class="left-div-paragraph">In general, naturopathic therapeutic protocols tend to be non-violent, non-toxic, non-invasive, and considered more in terms of achieving readjustment or reintegration. This may mean changes in diet, occupation, environment, or exercise wherever appropriate. I will obviously provide support in all these areas.</p>
+        </div>
+        </div>
+                                                        <!-- container-Experience -->
+<div class="head-container-experience">
+            <h1 class="support-h1">Experience and Interest:</h1><hr class="experience-hr">
+            <h2>My areas interest include:</h2>
+            <p>I specialise in female health conditions including supporting underlying imbalances which may contribute to the following conditions:</p>
+        </div>
+        <div class="container-Experience">
+            <div class="left-Experience">
+                <div>
+                <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                <span>Endometriosis</span></div>
+                <div>
+                <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                <span>Fibroids</span></div>
+                <div>
+                <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                <span>PCOS</span></div>
+                <div>
+                <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                <span>Thyroid health</span> <br></div>
+                
+            </div>
+            <div class="right-Experience">
+                <div>
+                    <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                    <span>Fertility issues</span></div>
+                    <div>
+                    <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                    <span>Integrative Oncology Support</span></div>
+                    <div>
+                    <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                    <span>PMS</span></div>
+                    <div>
+                    <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                    <span>Menopause, and peri-menopausal symptoms.</span> <br></div>
+            </div> 
+        </div>
+<br><br>
+                                                            <!-- CONTAINER TEXT -->
+        <div class="container-text">
+            <span class="text-container-NB">NB Always seek traditional medical advice with any diagnosed health condition first before exploring natural health options. <br>
+                I work alongside medical doctors and will liaise with them at your request.</span>
+        </div>
+
+
+                                                            <!-- container-Qualifications-Membership -->
+        <div class="container-Qualifications-Membership">
+                                                        <!-- ===================>left-Qualifications  -->
+            <div class="left-Qualifications">
+                <h1> Qualifications</h1>
+                <hr class="qual-memb-HR">
+                <div>
+                    <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                    <span>Naturopathic Nutritional Therapy Diploma and Naturopathy Diploma <br>
+                        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;(CNM – College of Naturopathic Medicine, London) 2019</span></div><br>
+                    <div>
+                    <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                    <span>Diploma of Higher Education Home Economics and Nutrition <br>
+                        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;(John Moore’s University, Liverpool) 2001</span></div><br>
+                    <div>
+                    <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                    <span>Natural Nutrition Diploma (College of Natural Nutrition, London) 2000</span></div><br>
+                    <div>
+                    <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                    <span>BTEC Higher National Diploma in Home Economics and Nutrition <br>
+                        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;(University of North London) 1993</span> <br></div>
+            </div>
+                                                            <!-- ===================>RIGHT MEMBERSHIP  -->
+            <div class="right-Membership">
+                <h1>Membership and Associations</h1>
+                <hr class="qual-memb-HR">
+                <div>
+                    <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                    <span>BSEM- British Society of Ecological Medicine</span></div>
+                    <br>
+                    <div>
+                    <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                    <span>CNHC – Complementary & Natural Healthcare Council</span></div><br>
+                    <div>
+                    <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                    <span>ANP- Association of Naturopathic Practitioners</span></div><br>
+                    <div>
+                    <span class="fa-solid fa-chevron-right" style="font-size: 36px; margin-right: 10px;"></span>
+                    <span>GNC – General Naturopathic Council (registered naturopath)</span> <br></div>
+                    
+            </div> 
+        </div>
+                                                                    <!-- CONTAINER 4 ALL -->
+        <div class="container4">
+        
+            <div class="anpimg">
+                <img src="https://www.emmamoorbynd.com/wp-content/uploads/2022/09/2020-members-badge.webp" alt="anpimg">
+            </div>
+            <div class="gncimg">
+                <img src="https://www.emmamoorbynd.com/wp-content/uploads/2022/09/GNC-LOGOnew-1024x305-1.webp" alt="gncimg">
+            </div>
+            <dir class="ecoimg">
+                <img src="https://www.emmamoorbynd.com/wp-content/uploads/2022/09/Screenshot-2021-06-02-155703.webp" alt="ecoimg">
+            </dir>
+        </div>
+                                                                <!-- button-Container  -->
+        <div class="button-Container">
+            <div class="btn-text">
+                <h1 class="btn-h1">Book a free discovery call</h1>
+                <p class="btn-p">Book your free discovery call today and follow us on Facebook for the latest news here.</p>
+            </div>
+            <div>
+                <button class="book-btnb">BOOK NOW</button>
+            </div>
+
+        </div>
+<br><br><br>
+
+    <style>
+
+    
+        .container {
+    display: flex;
+    flex-direction: row;
+    width: 1200px;
+    margin: 0 auto;
+}
+.text-column {
+    width: 75%;
+    padding: 20px;
+    
+}
+.image-column {
+    width: 75%;
+    padding: 20px;
+    order: -1;
+}
+img {
+    width: 100%;
+    height: auto;
+}
+.hr-natur{
+    width: 150px;
+    margin-left: 0;
+}
+h1{
+    font-family: 'Poppins', sans-serif;
+    font-weight: 300;
+    color: #535555;
+    line-height: 1.2;
+    
+}
+p{
+    color: #808285;
+    font-size: 2rem;
+    font: 1em sans-serif;
+    line-height: 1.9;
+    
+}
+.container-span{
+    font-size: 1em;
+    color: black;
+}
+
+.container2{
+    display: flex;
+    width: 100%;
+    background-color: #F4F5F0;
+    margin: 0 auto;
+    height: 250px;
+    flex-direction: row;
+    z-index: 1;
+}
+.left-div-2{
+    
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+    width: 30%;
+    height: 350px;
+    position:relative; left:80px;
+    padding: 50px;
+    z-index: 1;
+}
+.left-div-paragraph{
+    font-style: oblique;
+}
+.right-div-2{
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+    width: 30%;
+    height: 350px;
+    position:relative; right:80px;
+    padding: 50px;
+    z-index: 1;
+}
+.head-container-experience{
+    display: flex;
+    margin-top: auto;
+    align-items: center;
+    width: 100%;
+    padding-bottom: 10px;
+    flex-direction: column;
+}
+.experience-hr{
+    margin-right: auto;
+    width: 400px;
+}
+                    /* EXPERIANCE CONTAINER */
+.container-Experience{
+    display: flex;
+    width: 1200px;
+    margin: 0 auto;
+}
+.left-Experience{
+    width: 50%;
+    padding: 20px;
+}
+.right-Experience{
+    width: 50%;
+    padding: 20px;
+}
+.fa-solid {
+    margin-right: 10px;
+    vertical-align: middle;
+    color: rgb(204, 214, 208);
+}
+.container-text{
+    width:1200px;
+    display: flex;
+    margin: auto;
+    padding-left: 40px;
+}
+.text-container-NB{
+    font-size: 1.2em;
+}
+                    /* container-Qualifications-Membership */
+.container-Qualifications-Membership{
+    display: flex;
+    width: 100%;
+    margin: 0 auto;
+    background-color: #F4F5F0;
+}
+.left-Qualifications{
+    width: 50%;
+    padding: 50px;
+    margin: 0 auto;
+    position:relative; left:125px;
+    font-size: 1.2em;
+    font-style: oblique;
+}
+.right-Membership{
+    width: 50%;
+    padding: 50px;
+    margin: 0 auto;
+    position:relative; right: 20px;
+    font-size: 1.2em;
+    font-style: oblique;
+}
+.qual-memb-HR{
+    width: 150px;
+    margin-left: 0;
+}
+                                /* CONTAINER 4 ALL */
+.container4{
+    width: 1200px;
+    display: flex;
+    margin: 0 auto;
+}
+.anpimg{
+    width: 300px;
+    padding: 30px;
+    margin: 0 auto;
+}
+.gncimg{
+    width: 300px;
+    padding: 30px;
+    margin: 0 auto;
+}
+.ecoimg{
+    width: 300px;
+    padding: 30px;
+    margin: 0 auto;
+}
+                                /* button-container */
+.button-Container{
+    background-color: #c8ccb4;
+    width: 1100px;
+    display: flex;
+    flex-direction: row;
+    margin: 0 auto;
+    padding: 20px;
+    height: 100px;
+}
+.book-btnb{
+    border-radius: 22px;
+    background-color: #8fab72;
+    width: 200px;
+    height: 40px;
+    border-color: #8fab72;
+    color: white;
+    font-size: medium;
+    position:relative; left:280px; top:40px;
+    border: none;
+}
+.btn-p{
+    color: white;
+}
+.btn-h1{
+    color: white;
+}
+
+@keyframes slidInTop {
+    from{
+        transform: translateY(-300PX);
+    }
+    to{
+        transform: translateY(0);
+    }
+}
+
+.button-Container{
+    animation-name: slidInTop;
+    animation-duration: 1s;
+    animation-direction: alternate;
+}
+</style>
